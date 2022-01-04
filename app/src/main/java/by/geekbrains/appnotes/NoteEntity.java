715d1aac930e6be5ad5b9d7542749f404a3f2ee0
Oduvan58 <yancheuskipavel@gmail.com>
2022-01-04 15:14:46 +0300

    Crate data class for note

---
+++ app/src/main/java/by/geekbrains/appnotes/NoteEntity.java
@@ -0,0 +1,74 @@
+package by.geekbrains.appnotes;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class NoteEntity implements Parcelable {
+
+    private String title;
+    private String description;
+    private String date;
+
+    public NoteEntity(String title, String description, String date) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    public NoteEntity() {
+    }
+
+    protected NoteEntity(Parcel in) {
+        title = in.readString();
+        description = in.readString();
+        date = in.readString();
+    }
+
+    public static final Creator<NoteEntity> CREATOR = new Creator<NoteEntity>() {
+        @Override
+        public NoteEntity createFromParcel(Parcel in) {
+            return new NoteEntity(in);
+        }
+
+        @Override
+        public NoteEntity[] newArray(int size) {
+            return new NoteEntity[size];
+        }
+    };
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(title);
+        parcel.writeString(description);
+        parcel.writeString(date);
+    }
+}
