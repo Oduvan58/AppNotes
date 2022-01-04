936a54e5aab9698cf08466b08e8cd176cc63da70
Oduvan58 <yancheuskipavel@gmail.com>
2022-01-04 18:59:04 +0300

    Add RecyclerView

--- app/src/main/java/by/geekbrains/appnotes/MainActivity.java
+++ app/src/main/java/by/geekbrains/appnotes/MainActivity.java
@@ -1,14 +1,68 @@
 package by.geekbrains.appnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private NoteAdapter adapter;
+
+    private final ArrayList<NoteEntity> noteList = new ArrayList<>();
+    private static ArrayList<NoteEntity> createNotesData() {
+        final ArrayList<NoteEntity> noteEntities = new ArrayList<>();
+        noteEntities.add(new NoteEntity(
+                "Shop",
+                "Milk, juice, bread",
+                "04.01.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Meetings",
+                "Mother, brother, wife",
+                "07.01.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Travel",
+                "Spain, Germany, China",
+                "28.05.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Repair",
+                "Car, house, watch",
+                "10.03.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Job",
+                "Write a report, pass a test",
+                "19.01.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Investment",
+                "Bitcoin, tokens, stocks",
+                "24.04.2022"
+        ));
+        return noteEntities;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRecycler();
+    }
+
+    private void initRecycler() {
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+        adapter.setData(noteList);
     }
 }
---
+++ app/src/main/java/by/geekbrains/appnotes/NoteAdapter.java
@@ -0,0 +1,11 @@
+package by.geekbrains.appnotes;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends RecyclerView.Adapter {
+
+    public void setData(ArrayList<NoteEntity> noteList) {
+    }
+}
