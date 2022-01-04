46afe0ab6ab74d3b2b23c12d5dfef374b2237d9c
Oduvan58 <yancheuskipavel@gmail.com>
2022-01-04 21:54:49 +0300

    Add repository

---
+++ app/src/main/java/by/geekbrains/appnotes/data/CacheNoteRepositoryImpl.java
@@ -0,0 +1,60 @@
+package by.geekbrains.appnotes.data;
+
+import java.util.ArrayList;
+
+import by.geekbrains.appnotes.domain.NoteEntity;
+import by.geekbrains.appnotes.domain.NoteRepository;
+
+public class CacheNoteRepositoryImpl implements NoteRepository {
+
+    private final ArrayList<NoteEntity> cache = new ArrayList<>();
+
+    public CacheNoteRepositoryImpl() {
+        cache.addAll(createNotesData());
+    }
+
+    @Override
+    public ArrayList<NoteEntity> getNotes() {
+        return new ArrayList<>(cache);
+    }
+
+    @Override
+    public void deleteNote(NoteEntity noteEntity) {
+
+    }
+
+    private static ArrayList<NoteEntity> createNotesData() {
+        final ArrayList<NoteEntity> noteEntities = new ArrayList<>();
+        noteEntities.add(new NoteEntity(
+                "Shop",
+                "Milk, juice, bread",
+                "04.01.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Meetings",
+                "Mother, brother, wife",
+                "07.01.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Travel",
+                "Spain, Germany, China",
+                "28.05.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Repair",
+                "Car, house, watch",
+                "10.03.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Job",
+                "Write a report, pass a test",
+                "19.01.2022"
+        ));
+        noteEntities.add(new NoteEntity(
+                "Investment",
+                "Bitcoin, tokens, stocks",
+                "24.04.2022"
+        ));
+        return noteEntities;
+    }
+}
--- app/src/main/java/by/geekbrains/appnotes/ui/MainActivity.java
+++ app/src/main/java/by/geekbrains/appnotes/ui/MainActivity.java
@@ -6,58 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
-
 import by.geekbrains.appnotes.R;
-import by.geekbrains.appnotes.domain.NoteEntity;
+import by.geekbrains.appnotes.data.CacheNoteRepositoryImpl;
+import by.geekbrains.appnotes.domain.NoteRepository;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final NoteRepository noteRepository = new CacheNoteRepositoryImpl();
     private RecyclerView recyclerView;
     private NoteAdapter adapter;
-
-    private final ArrayList<NoteEntity> noteList = new ArrayList<>();
-
-    private static ArrayList<NoteEntity> createNotesData() {
-        final ArrayList<NoteEntity> noteEntities = new ArrayList<>();
-        noteEntities.add(new NoteEntity(
-                "Shop",
-                "Milk, juice, bread",
-                "04.01.2022"
-        ));
-        noteEntities.add(new NoteEntity(
-                "Meetings",
-                "Mother, brother, wife",
-                "07.01.2022"
-        ));
-        noteEntities.add(new NoteEntity(
-                "Travel",
-                "Spain, Germany, China",
-                "28.05.2022"
-        ));
-        noteEntities.add(new NoteEntity(
-                "Repair",
-                "Car, house, watch",
-                "10.03.2022"
-        ));
-        noteEntities.add(new NoteEntity(
-                "Job",
-                "Write a report, pass a test",
-                "19.01.2022"
-        ));
-        noteEntities.add(new NoteEntity(
-                "Investment",
-                "Bitcoin, tokens, stocks",
-                "24.04.2022"
-        ));
-        return noteEntities;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        noteList.addAll(createNotesData());
 
         initRecycler();
     }
@@ -67,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         adapter = new NoteAdapter();
-        adapter.setData(noteList);
+        adapter.setData(noteRepository.getNotes());
         recyclerView.setAdapter(adapter);
     }
 }
