1dd09b58ce075c2dd5a4a822148d152ebead1f0a
Oduvan58 <yancheuskipavel@gmail.com>
2022-01-29 15:15:15 +0300

    Add fragment about the app

---
+++ app/src/main/java/by/geekbrains/appnotes/ui/details/AboutFragment.java
@@ -0,0 +1,21 @@
+package by.geekbrains.appnotes.ui.details;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import by.geekbrains.appnotes.R;
+
+public class AboutFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+}
